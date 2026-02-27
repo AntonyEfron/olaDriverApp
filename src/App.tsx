68ede0bdede29536f8c36bf64b370f028ccf0ae6
Antonyefron <antonyefron007@gmail.com>
2026-02-27 13:17:00 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+﻿import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import { useScrollReveal } from './hooks/useScrollReveal';
+
+function App() {
+  useScrollReveal();
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
